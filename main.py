1312e1fe539333a9ab8639ffe5f0e14e529e9420
Dylan Byrne <dylan.byrne3454@gmail.com>
2026-02-07 16:17:34 +0000

    added 3 more algorythms

--- main.py
+++ main.py
@@ -103,7 +103,7 @@ def reconstruct_path(came_from, current, draw):
 		draw()
 
 
-def algorythm(draw, grid, start, end):
+def a_star(draw, grid, start, end):
 	count = 0
 	open_set = PriorityQueue()
 	open_set.put((0,count, start))
@@ -111,7 +111,7 @@ def algorythm(draw, grid, start, end):
 	g_score = {spot: float('inf') for row in grid for spot in row}
 	g_score[start] = 0
 	f_score = {spot: float('inf') for row in grid for spot in row}
-	g_score[start] = h(start.get_pos(), end.get_pos())
+	f_score[start] = h(start.get_pos(), end.get_pos())
 
 	open_set_hash = {start}
 
@@ -148,6 +148,123 @@ def algorythm(draw, grid, start, end):
 
 	return False
 
+def bfs(draw, grid, start, end):
+	queue = [start]
+	came_from = {}
+	visited = {start}
+
+
+
+	while queue:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				pygame.quit()
+
+		current = queue.pop(0)
+		
+		if current == end:
+			reconstruct_path(came_from, end, draw) 
+			end.make_end()
+			return True  
+
+		for neighbor in current.neighbors:
+			if neighbor not in visited:
+				visited.add(neighbor)
+				came_from[neighbor] = current
+				queue.append(neighbor)
+				neighbor.make_open()
+
+		draw()
+
+		if current != start:
+			current.make_closed()
+
+	return False
+
+def dijkstra(draw, grid, start, end):
+	count = 0
+	open_set = PriorityQueue()
+	open_set.put((0,count, start))
+	came_from = {}
+
+
+	g_score = {spot: float('inf') for row in grid for spot in row}
+	g_score[start] = 0
+	
+	open_set_hash = {start}
+
+	while not open_set.empty():
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				pygame.quit()
+
+		current = open_set.get()[2]
+		open_set_hash.remove(current)
+
+		if current == end:
+			reconstruct_path(came_from, end, draw) 
+			end.make_end()
+			return True  
+
+		for neighbor in current.neighbors:
+			temp_g_score = g_score[current] + 1
+
+			if temp_g_score < g_score[neighbor]:
+				came_from[neighbor] = current
+				g_score[neighbor] = temp_g_score
+
+				if neighbor not in open_set_hash:
+					count += 1
+					open_set.put((g_score[neighbor], count, neighbor))
+					open_set_hash.add(neighbor)
+					neighbor.make_open()
+
+		draw()
+
+		if current != start:
+			current.make_closed()
+
+	return False
+
+def greedy_best_first(draw, grid, start, end):
+	count = 0
+	open_set = PriorityQueue()
+	open_set.put((0, count, start))
+	came_from = {}
+	open_set_hash = {start}
+	visited = set()
+	
+	while not open_set.empty():
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				pygame.quit()
+		
+		current = open_set.get()[2]
+		open_set_hash.remove(current)
+		
+		if current in visited:
+			continue
+		visited.add(current)
+
+		if current == end:
+			reconstruct_path(came_from, end, draw) 
+			end.make_end()
+			return True  
+		
+		for neighbor in current.neighbors:
+			if neighbor not in open_set_hash and neighbor not in visited:
+				came_from[neighbor] = current
+				count += 1
+				h_score = h(neighbor.get_pos(), end.get_pos())
+				open_set.put((h_score, count, neighbor)) 
+				open_set_hash.add(neighbor)
+				neighbor.make_open()
+		
+		draw()
+		if current != start:
+			current.make_closed()
+	
+	return False
 
 def make_grid(rows, width):
 	grid = []
@@ -189,6 +306,11 @@ def get_clicked_pos(pos, rows, width):
 
 	return row, col 
 
+def run_algorythm(algorythm, draw_func, grid, start, end):
+	for row in grid:
+		for spot in row:
+			spot.update_neighbors(grid)
+	algorythm(draw_func, grid, start, end)
 
 def main(win, width):
 	grid = make_grid(ROWS, width)
@@ -233,17 +355,23 @@ def main(win, width):
 					end = None
 
 			if event.type == pygame.KEYDOWN:
-				if event.key == pygame.K_SPACE and start and end:
-					for row in grid:
-						for spot in row:
-							spot.update_neighbors(grid)
 
-					algorythm(lambda: draw(win, grid, ROWS, width), grid, start, end)
-			
 				if event.key == pygame.K_c:
 					start = None
 					end = None
 					grid = make_grid(ROWS, width)
+
+				elif start and end:
+
+					if event.key == pygame.K_a:
+						run_algorythm(a_star, lambda: draw(win, grid, ROWS, width), grid, start, end)
+					elif event.key == pygame.K_b:
+						run_algorythm(bfs, lambda: draw(win, grid, ROWS, width), grid, start, end)
+					elif event.key == pygame.K_d:
+						run_algorythm(dijkstra, lambda: draw(win, grid, ROWS, width), grid, start, end)
+					elif event.key == pygame.K_g:
+						run_algorythm(greedy_best_first, lambda: draw(win, grid, ROWS, width), grid, start, end)
+		
 
 	pygame.quit()
 
